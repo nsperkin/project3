68730e683cf0b0e25d98d4230afcd62bb7bc13a1
Nathaniel Perkins <nsperkins13@gmail.com>
2019-07-25 16:18:45 -0700

    Created Models and Populated DB

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
 # Create your models here.
+class Topping(models.Model):
+	topping = models.CharField(max_length=64)
+
+	def __str__(self):
+		return self.topping
+
+class Pizza(models.Model):
+	style = models.CharField(max_length=64)
+	size = models.CharField(max_length=64)
+	num_toppings = models.IntegerField()
+	price = models.FloatField()
+
+	def __str__(self):
+		return f"{self.size} {self.style} Pizza with {self.num_toppings} topping(s)"
+
+class Pizza_Order(models.Model):
+	pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+	toppings = models.ManyToManyField(Topping, blank=True)
+
+	def __str__(self):
+		return f"{self.pizza}"
+
+class Sub(models.Model):
+	style = models.CharField(max_length=64)
+	price = models.FloatField()
+	size = models.CharField(max_length=64)
+
+	def __str__(self):
+		return f"{self.size} {self.style} sub"
+
+class Sub_Order(models.Model):
+	sub = models.ForeignKey(Sub, on_delete=models.CASCADE)
+	extra_cheese = models.BooleanField(default=False)
+	extra_muchrooms = models.BooleanField(default=False)
+	extra_green_peppers = models.BooleanField(default=False)
+	extra_onions = models.BooleanField(default=False)
+	
+
+	def __str__(self):
+		return self.sub
+
+class Pasta(models.Model):
+	pasta = models.CharField(max_length=64)
+	price = models.FloatField()
+
+	def __str__(self):
+		return f"{self.pasta} pasta"
+
+class Salad(models.Model):
+	salad = models.CharField(max_length=64)
+	price = models.FloatField()
+
+	def __str__(self):
+		return self.salad
+
+class Dinner_Platter(models.Model):
+	dinner_platter = models.CharField(max_length=64)
+	price = models.FloatField()
+	size = models.CharField(max_length=64)
+
+	def __str__(self):
+		return f"{self.size} {self.dinner_platter}"
+
+class Order(models.Model):
+	pizza_orders = models.ManyToManyField(Pizza_Order, blank=True)
+	sub_orders = models.ManyToManyField(Sub_Order, blank=True)
+	pastas = models.ManyToManyField(Pasta, blank=True)
+	salads = models.ManyToManyField(Salad, blank=True)
+	dinner_platters = models.ManyToManyField(Dinner_Platter, blank=True)
+
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+	context = {
+		"pizza": Pizza.objects.all()
+	}
+	return render(request, "orders/index.html", context)
